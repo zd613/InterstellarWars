747b6d7ce15d24f3419b2deeb0675c1a7faaf88f
ame <25921047+zd613@users.noreply.github.com>
2019-01-20 20:36:16 +0900

    惑星のスクリプト作成 一定時間ごとに数が増えるようにした

---
+++ Assets/InterstellarWars/Scripts/Planet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    //パラメータ
+    public int maxShips = 10;//船がこの星にいられる最大数
+    public int increasementPerSecond;//1秒当たりに増える船の数
+
+
+    public int currentNumberOfShips = 0;//現在の船数
+
+    float deltaTimeToIncrease;
+    float time = 0;
+
+    private void Awake()
+    {
+        deltaTimeToIncrease = 1.0f / increasementPerSecond;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time > deltaTimeToIncrease)
+        {
+            while (true)
+            {
+                var t = time - deltaTimeToIncrease;
+
+                if (t > 0)
+                {
+                    time = t;
+                    if (currentNumberOfShips < maxShips)
+                    {
+                        currentNumberOfShips += 1;
+                    }
+                    continue;
+                }
+                break;
+            }
+        }
+        print(currentNumberOfShips);
+    }
+
+}
