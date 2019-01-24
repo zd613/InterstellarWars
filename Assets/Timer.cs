d3794b52ecde9d76e6b9f69cc15e0723a5230591
ame <25921047+zd613@users.noreply.github.com>
2019-01-24 16:55:05 +0900

    player manager,planet,timerプレハブ化

---
+++ Assets/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 60;//カウントダウン開始時間
+
+
+    float timeLeft = 0;
+
+    private void Awake()
+    {
+        timeLeft = time;
+    }
+
+    private void Update()
+    {
+        timeLeft -= time;
+        if (timeLeft < 0)
+        {
+            timeLeft = 0;
+            print("end");
+
+
+            //後で消す
+            timeLeft = time;
+        }
+    }
+}
